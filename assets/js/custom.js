289beef539a2ff393b89479a3aba49c79caa1797
ahyaniayas <ahyani20031998@gmail.com>
2020-08-07 11:37:42 +0700

    custom js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,5 +3,13 @@ function numberWithCommas(x){
 }
 
 $(".number").keyup(function(){
-	$(this).val(numberWithCommas($(this).val().replace(/,/g, "")));
+	var val = $(this).val().toString().replace(/,/g, "");
+	for(var i=0; i<1; i++){
+		var awal = val.toString().substr(0, 1);
+		if(awal=="0"){
+			i = 0;
+			val = val.toString().substr(1, val.length);
+		}
+	}
+	$(this).val(numberWithCommas(val));
 })
